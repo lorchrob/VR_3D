9887c8ca06f4f84c820b95e616086c27e616e217
Robert Lorch <lorchrob@gmail.com>
2020-09-20 12:31:43 -0400

    Refine linear transformation gun and HUD

--- Assets/Scripts/BlenderMesh.cs
+++ Assets/Scripts/BlenderMesh.cs
@@ -6,34 +6,46 @@ public class BlenderMesh : MonoBehaviour
 {
     Mesh mesh;
     Vector3[] vertices;
-    //Vector3[] originalVertices;
-  
+    Vector3[] targetVertices;
 
     // Start is called before the first frame update
     void Start()
     { 
         mesh = GetComponent<MeshFilter>().mesh;
         vertices = mesh.vertices;
-        //originalVertices = mesh.vertices;
+        targetVertices = mesh.vertices;
+    }
+
+    void Update()
+    {
+
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            vertices[i].x += (targetVertices[i].x - vertices[i].x) * Time.deltaTime * 4;
+            vertices[i].y += (targetVertices[i].y - vertices[i].y) * Time.deltaTime * 4;
+            vertices[i].z += (targetVertices[i].z - vertices[i].z) * Time.deltaTime * 4;
+
+            mesh.vertices = vertices;
+            GetComponent<MeshCollider>().sharedMesh = mesh;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+        }
     }
 
 
-    public void TransformShape(TransMatrix matrix)
+    public void TransformShape(Vector3[] matrix)
     {
         for (int i = 0; i < mesh.vertices.Length; i++)
         {
-            //print(mesh.vertices[i].x + " " + mesh.vertices[i].y + " " + mesh.vertices[i].z);
-            float x = vertices[i].x;
-            float y = vertices[i].y;
-            float z = vertices[i].z;
-            vertices[i] = new Vector3(x * matrix.matrix[0].x + y * matrix.matrix[0].y + z * matrix.matrix[0].z,
-                                      x * matrix.matrix[1].x + y * matrix.matrix[1].y + z * matrix.matrix[1].z,
-                                      x * matrix.matrix[2].x + y * matrix.matrix[2].y + z * matrix.matrix[2].z);
+            float x = targetVertices[i].x;
+            float y = targetVertices[i].y;
+            float z = targetVertices[i].z;
+            targetVertices[i] = new Vector3(x * matrix[0].x + y * matrix[0].y + z * matrix[0].z,
+                                      x * matrix[1].x + y * matrix[1].y + z * matrix[1].z,
+                                      x * matrix[2].x + y * matrix[2].y + z * matrix[2].z);
         }
 
-        mesh.vertices = vertices;
-        GetComponent<MeshCollider>().sharedMesh = mesh;
-        mesh.RecalculateNormals();
     }
 
 }
--- Assets/Scripts/TransMatrix.cs
+++ Assets/Scripts/TransMatrix.cs
@@ -9,5 +9,31 @@ public class TransMatrix : ScriptableObject
     
     public Vector3[] matrix = { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) };
 
+    public Vector3[] invertMatrix = { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) };
+
+    public void calcInverse()
+    {
+
+        float det = matrix[0].x * (matrix[2].z * matrix[1].y - matrix[1].z * matrix[2].y)
+            - matrix[0].y * (matrix[1].x * matrix[2].z - matrix[1].z * matrix[2].x)
+            + matrix[0].z * (matrix[2].y * matrix[1].x - matrix[1].y * matrix[2].x);
+
+        invertMatrix[0].x = (1 / det) * (matrix[2].z * matrix[1].y - matrix[1].z * matrix[2].y);
+        invertMatrix[0].y = (1 / det) * (matrix[2].y * matrix[0].z - matrix[0].y * matrix[2].z);
+        invertMatrix[0].z = (1 / det) * (matrix[0].y * matrix[1].z - matrix[0].z * matrix[1].y);
+
+        invertMatrix[1].x = (1 / det) * (matrix[1].z * matrix[2].x - matrix[1].x * matrix[2].z);
+        invertMatrix[1].y = (1 / det) * (matrix[0].x * matrix[2].z - matrix[0].z * matrix[2].x);
+        invertMatrix[1].z = (1 / det) * (matrix[0].z * matrix[1].x - matrix[0].x * matrix[1].z);
+
+        invertMatrix[2].x = (1 / det) * (matrix[1].x * matrix[2].y - matrix[1].y * matrix[2].x);
+        invertMatrix[2].y = (1 / det) * (matrix[0].y * matrix[2].x - matrix[0].x * matrix[2].y);
+        invertMatrix[2].z = (1 / det) * (matrix[0].x * matrix[1].y - matrix[0].y * matrix[1].x);
+
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(invertMatrix[i].x.ToString() + ", " + invertMatrix[i].y.ToString() + ", " + invertMatrix[i].z.ToString());
+        }
+    }
 
 }
